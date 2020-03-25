e50be0decf3ccbd615b78664bf47f904545fb9b3
Gabriel Neri <neri.gabriel78@gmail.com>
2020-03-25 16:59:48 -0300

    ajustes para pegar a pergunta e esquema de rodada

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -13,14 +13,23 @@ export class PainelComponent implements OnInit {
   public instrucao: string = "Traduza a frase: ";
   public resposta: string;
 
-  constructor() { }
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada];
+  }
 
   ngOnInit() {}
 
   //$event no html = recupera o estado do html no DOM.
   public atualizaResposta(valor: Event) : void {
     this.resposta = (<HTMLInputElement>valor.target).value;
-    console.log( this.resposta);
+    //console.log( this.resposta);
+  }
+
+  public verificaResposta() : void {
+    console.log('Verificar resposta', this.resposta);
   }
 
 }
