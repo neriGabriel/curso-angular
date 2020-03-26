37cd2c95eeebb7d7dcb0252531c183a69fb9a75f
Gabriel Neri <neri.gabriel78@gmail.com>
2020-03-26 18:50:51 -0300

    Add: progress bar

--- src/app/painel/frase.mock.ts
+++ src/app/painel/frase.mock.ts
@@ -5,6 +5,4 @@ export const FRASES: Array<Frase> = [
     {fraseEng: 'I like to learn', frasePt: 'Eu gosto de aprender'},
     {fraseEng: 'I watch tv', frasePt: 'Eu assisto tv'},
     {fraseEng: 'I eat bread', frasePt: 'Eu como pão'},
-    {fraseEng: 'How are you?', frasePt: 'Como vai você?'},
-    {fraseEng: 'My life is good', frasePt: 'Minha vida é boa'},
 ];
--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -16,6 +16,8 @@ export class PainelComponent implements OnInit {
   public rodada: number = 0;
   public rodadaFrase: Frase;
 
+  public progresso: number = 0;
+  
   constructor() { 
     this.rodadaFrase = this.frases[this.rodada];
   }
@@ -25,11 +27,25 @@ export class PainelComponent implements OnInit {
   //$event no html = recupera o estado do html no DOM.
   public atualizaResposta(valor: Event) : void {
     this.resposta = (<HTMLInputElement>valor.target).value;
-    //console.log( this.resposta);
   }
 
   public verificaResposta() : void {
-    console.log('Verificar resposta', this.resposta);
+    //trocar pergunta da rodada
+    if(this.rodadaFrase.frasePt === this.resposta ) {
+        this.progresso = this.progresso + (100 / this.frases.length);
+        console.log( this.progresso);
+        this.rodada++;
+
+        if(this.rodada >= this.frases.length) {
+          this.rodada = 0;
+        }
+
+        this.rodadaFrase = this.frases[this.rodada];
+        this.resposta = '';
+    } else {
+        alert('Frase traduzida incorretamente');
+    }
+    
   }
 
 }
